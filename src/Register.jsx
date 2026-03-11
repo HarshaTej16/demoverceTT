2efe67426b12e1dd19aee5a3051f28c55c90b045
harsha16 <bvani2530@gmail.com>
2026-03-11 13:42:26 +0530

    Update registration endpoint URL

--- src/Register.jsx
+++ src/Register.jsx
@@ -13,7 +13,7 @@ function Reg() {
   const submit = async () => {
     try {
       //const res = await axios.post("http://localhost:8000/reg", data);
-      const res = await axios.post("https://new-g5f1.onrender.com/reg", data);  
+      const res = await axios.post("https://new-g5f1.onrender.com/register", data);  
       alert(res.data);
     } catch (xyz) {
       alert(xyz.response.data);
